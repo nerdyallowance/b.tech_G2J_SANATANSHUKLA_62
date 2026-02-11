1a7339b474f725cc64075db4055bc51b159f5a80
nerdyallowance <118093630+nerdyallowance@users.noreply.github.com>
2026-02-11 10:01:36 +0530

    Add files via upload
    ye meri pehli asi ha (kind off)
    par ho gaya bhai

---
+++ hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+
+float a , b;
+     printf("Enter first number: ");
+     scanf("%d" , &a);
+     printf("Enter second number: ");
+     scanf("%d" , &b);
+     
+    // sum = hindi + english + science + math + computer;
+      // percentage = (sum / 5) * 100;
+
+     //printf("Total: %d", sum);
+     //printf("Percentage = %.2f%%\n" , percentage);
+if (a == b) {
+printf("numbers entered by user are equal");
+} 
+//if (percentage >= 80) {
+//printf("Grade: B");
+
+else {
+printf("both numbers are not equal");
+}
+  
+ return 0;
+}
